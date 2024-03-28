166bb524ae59ff5721614e24e4f6f9ac0fdc9451
Documenter.jl <documenter@juliadocs.github.io>
2024-03-28 14:00:11 +0000

    build based on 56cdbc1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SomeStructuredMatrices","category":"page"},{"location":"#SomeStructuredMatrices","page":"Home","title":"SomeStructuredMatrices","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SomeStructuredMatrices.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SomeStructuredMatrices]","category":"page"}]
+}
